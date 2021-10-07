674056e90b26bae0db692a783d43e113d054e904
Klara Sakmarova <xsakma00@vutbr.cz>
2021-10-07 14:24:57 +0200

    iterace

--- rekurze.R
+++ rekurze.R
@@ -1,7 +1,7 @@
 # cokoladova cesta 
 Cokolada <- function(M,r,s){
  if (r == nrow(M)){
- return (M(r,s))
+ return (M(r,s))     # ukonceni 
  }
  else {
    C <- M(r,s)
@@ -9,4 +9,20 @@ Cokolada <- function(M,r,s){
  Cdolu <- Cokolada(M,r+1,s)
  Csikmo <- Cokolada(M,r+1,s+1)
  return (max(Cdolu, Csikmo) + C)
+}
+
+
+# cokoladova cesta - iteraciou
+Cokolada_2 <- function(M){
+  s <- dim(M)
+  k1 <- seq (from=s[1]-1,to=1,by=-1)
+  for (r in k1) {
+    k2 <- seq (from=r,to=1,by=-1)
+    for (s in k2) {
+      Cdolu <- M[r+1,s]+M[r,s]
+      Csikmo <- M[r+1,s+1]+M[r,s]
+      M[r,s] <- max(c(Cdolu,Csikmo))
+    }   
+}
+return (M[1,1])
 }
